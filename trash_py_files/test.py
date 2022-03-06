80f0eb0d878e0c3811c75bfbb12a7d63f39dbbac
Oleksandr Rapii <oleksandr.rapii.c@greenwayhealth.com>
2022-03-06 23:23:34 +0200

    update test.py file

--- trash_py_files/test.py
+++ trash_py_files/test.py
@@ -1,8 +1,30 @@
+import random
+
+
 # using of array
 array_data = [1, 'some', 43.2, 's']
-for i in array_data: 
-    print(type(i))
+
+# for i in array_data: 
+#     print(type(i))
 
 # using of range
 list_range = list(range(1, 10))
-print(list_range)
+# print(list_range)
+
+
+random_list = random.sample(range(10, 100), 5)
+
+print(random_list)
+print(min(random_list))
+
+
+dictionary = {'some': 1, 'some2': 2, 'another': 5}
+
+for item in dictionary:
+    print(dictionary[item])
+
+print(dictionary.values())
+
+day_temperatures = {'morning':  (3.3,4.4, 42.3), 'noon': (3.3,4.4, 42.3), 'evening':  (3.3,4.4, 42.3) }
+
+print(day_temperatures.values())
